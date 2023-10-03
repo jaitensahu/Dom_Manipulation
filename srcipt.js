11fc602e41e91c4d0b11daf379ec9738069c0bd3
Jaiten <Jaitensahu9@gmail.com>
2023-10-03 18:41:05 +0530

    added file

---
+++ srcipt.js
@@ -0,0 +1,12 @@
+let div=document.getElementById("container");
+div.style.backgroundColor="yellow";
+div.style.margin="20px";
+div.style.padding="10px";
+div.style.height="200px";
+div.style.width="300px";
+
+let text=document.getElementById("paragraph");
+text.style.color="blue";
+text.style.fontSize="18px";
+text.style.fontWeight="bold";
+
